32555224aa24b5b426f3f8add2d51099bb22661d
PabloChimi <pablo.chimi@hotmail.com>
2017-07-02 12:07:21 -0300

    guia if terminada

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,13 @@
 function Mostrar()
 {
-	//Genero el número RANDOM entre 1 y 10 
-	
-
+	//Genero el número RANDOM entre 1 y 10
+	var myRandom;
+	myRandom = Math.floor((Math.random() * 10) + 1);
+  if(myRandom>=9){alert("excelente");}
+	else {
+		if(myRandom>=4){alert("aprobo");}
+		else {
+			alert("Vamos, la proxima se puede");
+		}
+	}
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (8).js
+++ 2-InstruccionIf/jsInstruccionIF (8).js
@@ -1,8 +1,10 @@
 function Mostrar()
 {
-//tomo la edad  
-
-	
-
-
+	var edadRecibida, estadoCivilRecibido;
+  edadRecibida = parseInt(document.getElementById("edad").value);
+	estadoCivilRecibido = document.getElementById("estadoCivil").value;
+  if(edadRecibida >= 18 && estadoCivilRecibido=="Soltero"){
+		alert("Es soltero y no es menor");
+	}
+//tomo la edad
 }//FIN DE LA FUNCIÓN
